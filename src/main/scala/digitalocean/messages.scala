fc93858af4681e4d1a444d1a69747a78a7e5506d
Jeffrey N. Davis <Jeffrey.N.Davis@gmail.com>
2016-05-21 13:35:18 -0500

    Added Messages and Commands.
    Commands represented by INDEX | REMOVE | QUERY are now represented in
    the type system. A type Message was introduced to represent the
    combination of a Command + package the command is related to + any
    dependencies that package has.
    
    In addition, Message introduced a Decoding method that returns a Try
    parameterized on the Message itself.

--- src/main/scala/digitalocean/messages.scala
+++ src/main/scala/digitalocean/messages.scala
@@ -2,9 +2,49 @@ package digitalocean
 package packagetree
 
 import java.nio.charset.StandardCharsets
+import scala.util.Try
+import digitalocean.packagetree.errors.InvalidProtocolError
 
 object messages {
 
+  sealed abstract class Command
+  final case object Index extends Command
+  final case object Remove extends Command
+  final case object Query extends Command
+
+  case class Message(command: Command, pkg: String, dependencies: List[String])
+  object Message {
+    private[packagetree] val Delimiter = '|' // private to packagetree for testing
+
+    def apply(xs: String): Try[Message] = Try({
+      val pipeCount = xs.foldLeft(0)((i,j) => j match {
+        case Delimiter => i + 1
+        case _         => i
+      })
+      if(pipeCount != 2) 
+        throw new InvalidProtocolError(s"Invalid delimiter count - $pipeCount delimiters found.")
+      val command = xs.takeWhile(_ != Delimiter ).mkString match {
+        case "INDEX"  => Index
+        case "REMOVE" => Remove
+        case "QUERY"  => Query
+        case cmd      => throw new InvalidProtocolError(s"Unknown command $cmd")
+      }
+      val firstIdx = xs.indexOf(Delimiter)
+      val lastIdx  = xs.lastIndexOf(Delimiter)
+      val pkg = xs.slice(firstIdx + 1, lastIdx).mkString
+      val depLength = xs.length - (lastIdx + 1)
+      val dependencies = depLength match {
+        case x if x > 1  => xs.takeRight(xs.length - (lastIdx + 1))
+          .mkString
+          .stripLineEnd
+          .split(",")
+          .toList
+        case _           => List.empty[String]
+      }
+
+      Message(command, pkg, dependencies)
+    })
+  }
 
   sealed abstract class Response() {
     private[this] val okBytes    = "OK\n".getBytes(StandardCharsets.UTF_8)
